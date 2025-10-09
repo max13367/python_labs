93219dd8949ed46b32e667d36e3dd00636b8edae
max13367 <maksim.batrachenko@gmail.com>
2025-10-10 01:02:13 +0300

    лаба 2

--- lab02/03 2.py
+++ lab02/03 2.py
@@ -1,24 +1,26 @@
-def format_record(student, gruppa, gpa):
-    student = student.title()
+def format_record(rec: tuple[str, str, float]) -> str:
+    student, gruppa, gpa = rec
+
+    student = ' '.join(student.split()).title()
+
     parts = student.split()
     familiya = parts[0]
-    new_fam = []
-    if len(parts) > 1:
-        new_fam.append(parts[1])
-    if len(parts) > 2:
-        new_fam.append(parts[2])
+
 
     initial = []
-    for i in new_fam:
-        initial.append(i[0] + '.')
-
+    for i in range(1, len(parts)):
+        if parts[i]:
+            initial.append(parts[i][0] + '.')
 
     initials_str = ''.join(initial)
-    fio = f'{familiya} {initials_str}, гр. {gruppa}, GPA {gpa:.2f}'
+
+
+    fio = f'{familiya} {initials_str}, гр. {gruppa.strip()}, GPA {gpa:.2f}'
     return fio
 
 
-print(format_record("Иванов Иван Иванович", "BIVT-25", 4.6))
-print(format_record("Петров Пётр", "IKBO-12", 5.0))
-print(format_record("Петров Пётр Петрович", "IKBO-12", 5.0))
-print(format_record("  сидорова  анна   сергеевна ", "ABB-01", 3.999))
+# Тестирование
+print(format_record(("Иванов Иван Иванович", "BIVT-25", 4.6)))
+print(format_record(("Петров Пётр", "IKBO-12", 5.0)))
+print(format_record(("Петров Пётр Петрович", "IKBO-12", 5.0)))
+print(format_record(("  сидорова  анна   сергеевна ", "ABB-01", 3.999)))
